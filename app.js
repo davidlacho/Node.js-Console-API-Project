22fc41ceb5240f27d1d01f5625aadd6aaed3be2d
David Lacho <david.lacho@gmail.com>
2018-01-18 08:46:54 -0800

    Error Handling
    Added try block to the https request. This handles error in case the
    argument are good but fails at runtime with asynchronous error (for
    example, argument is http:// rather than https://).

--- app.js
+++ app.js
@@ -10,37 +10,41 @@ const rl = readline.createInterface({
 rl.question('What is the username? ', (username) =>{
   console.log(`Connecting to teamTreehouse...`);
   console.log(`Requesting User data for ${username}...`);
-  let requestURL = `https://teamtreehouse.com/${username}.json`
-  const https = require('https');
-  const request = https.get(requestURL, (res) => {
-    const { statusCode } = res;
-      if (statusCode === 200) {
-        console.log(`Success! Data retrieved for ${username}`);
-        let rawData= '';
-        res.on('data', (chunk) => {
-          rawData += chunk.toString();
+  try{
+    let requestURL = `https://teamtreehouse.com/${username}.json`
+    const https = require('https');
+    const request = https.get(requestURL, (res) => {
+      const { statusCode } = res;
+        if (statusCode === 200) {
+          console.log(`Success! Data retrieved for ${username}`);
+          let rawData= '';
+          res.on('data', (chunk) => {
+            rawData += chunk.toString();
+          });
+          res.on('end', () => {
+            console.log('Reading User Data...');
+          try{
+            const parsedData = JSON.parse(rawData);
+            let badgeCount = parsedData.badges.length;
+            let points= parsedData.points.JavaScript;
+            printMessage(username, badgeCount, points);
+          } catch (e) {
+            console.error(`Error in JSON parsing: ${e.message}`);
+          }
         });
-        res.on('end', () => {
-          console.log('Reading User Data...');
-        try{
-          const parsedData = JSON.parse(rawData);
-          let badgeCount = parsedData.badges.length;
-          let points= parsedData.points.JavaScript;
-          printMessage(username, badgeCount, points);
-        } catch (e) {
-          console.error(`Error in JSON parsing: ${e.message}`);
+      } else {
+        const { statusMessage } = res;
+        console.error(`Server responded with error: ${statusCode}: ${statusMessage}`)
+        if (statusCode === 404){
+          console.error(`User ${username} may not exist, or their profile may be set to private.`);
         }
-      });
-    } else {
-      const { statusMessage } = res;
-      console.error(`Server responded with error: ${statusCode}: ${statusMessage}`)
-      if (statusCode === 404){
-        console.error(`User ${username} may not exist, or their profile may be set to private.`);
       }
-    }
-  }).on('error', (e)=> {
-    console.error(`https.get error: ${e.message}`);
-  });
+    }).on('error', (e)=> {
+      console.error(`https.get error: ${e.message}`);
+    });
+  } catch (e) {
+    console.error(`Asynchronous error in https request: ${e.message}`);
+  }
   rl.close();
 });
 
